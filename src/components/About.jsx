740b9159cfdc1e33b1e93b3c50dfd7f69a442865
Evan S Jones <esj.ejones@gmail.com>
2025-01-23 14:09:35 -0500

    corrected commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,12 @@
-import { Component } from "react";
 import '../PortfolioStyles.css';
 
-class About extends Component {
-    render () {
-        return (
-            <section>
-                <h2>About Me</h2>
-                <p>I am a beginner Web Developer embarking on a journey to master Python, HTML, CSS, Bootstrap, JavaScript, and React. Come along and learn with me!</p>
-            </section>
-        );
-    }
-}
+const About = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>I am a beginner Web Developer embarking on a journey to master Python, HTML, CSS, Bootstrap, JavaScript, and React. Come along and learn with me!</p>
+        </section>
+    );
+};
 
 export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,16 @@
+import { Component } from "react";
 import '../PortfolioStyles.css';
 
-const Contact = () => {
-    return (
-        <footer>
-            <h2>Contact</h2>
-            <p>Email: esj.ejones@gmail.com</p>
-            <p>LinkedIn: linkedin.com/in/evansjones</p>
-        </footer>
-    );
-};
+class Contact extends Component {
+    render () {
+        return (
+            <footer>
+                <h2>Contact</h2>
+                <p>Email: esj.ejones@gmail.com</p>
+                <p>LinkedIn: linkedin.com/in/evansjones</p>
+            </footer>
+        );
+    }
+}
 
 export default Contact;
